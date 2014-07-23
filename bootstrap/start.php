2a6afa805b048d8d8b68a8e4ee7f43d7fa9be12a
Ethan Wortzman <ethanwortzman@g.harvard.edu>
2014-07-23 12:23:57 -0400

    formatting

--- bootstrap/start.php
+++ bootstrap/start.php
@@ -25,7 +25,7 @@ $app = new Illuminate\Foundation\Application;
 */
 
 $env = $app->detectEnvironment(array(
-		# See if there's an environment.php file; if it does, get environment string from there
+    # See if there's an environment.php file; if it does, get environment string from there
     if(file_exists(__DIR__.'/environment.php')) {
         return require __DIR__.'/environment.php';
     }
