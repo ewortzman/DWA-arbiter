415c84a198b2f572dc721e6674379b97310d8b77
Ethan Wortzman <ethanwortzman@g.harvard.edu>
2014-07-26 00:09:43 -0400

    creted skeleton for routes.php

--- app/routes.php
+++ app/routes.php
@@ -13,5 +13,89 @@
 
 Route::get('/', function()
 {
-	return View::make('hello');
+	return "homepage placeholder";
+});
+
+Route::get('/login', function(){
+	return "login placeholder";
+});
+
+Route::post('/login', function(){
+	return "login POST placeholder";
+});
+
+Route::get('/dashboard', function(){
+	return "dashboard placeholder";
+});
+
+Route::get('/association/{id}', function($id){
+	return "association placeholder";
+});
+
+Route::post('/association/{id}', function($id){
+	return "association POST placeholder";
+});
+
+Route::get('/association/{id}/members', function($id){
+	return "association member list placeholder";
+});
+
+Route::get('/association/{id}/schedule', function($id){
+	return "association master schedule placeholder";
+});
+
+Route::get('/schedule', function(){
+	return "schedule placeholder";
+});
+
+Route::get('/profile', function(){
+	return "profile placeholder";
+});
+
+Route::post('/profile', function(){
+	return "profile POST placeholder";
+});
+
+Route::get('/user/{id}', function($id){
+	return "user page placeholder";
+});
+
+Route::get('/event/{id}', function($id){
+	return "event page placeholder";
+});
+
+Route::post('/event/{id}', function($id){
+	return "event page POST placeholder";
+});
+
+Route::get('/event/add', function(){
+	return "add event placeholder";
+})
+
+Route::post('/event/add', function(){
+	return "add event POST placeholder";
+})
+
+Route::get('/school/{id}', function($id){
+	return "school page placeholder";
+});
+
+Route::post('/school/{id}', function($id){
+	return "school page POST placeholder";
+});
+
+Route::get('/team/{id}', function($id){
+	return "team page placeholder";
+});
+
+Route::post('/team/{id}', function($id){
+	return "team page POST placeholder";
+});
+
+Route::get('/blocks', function(){
+	return "blocks placeholder";
+});
+
+Route::post('/blocks', function(){
+	return "blocks POST placeholder";
 });
