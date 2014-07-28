431ba0aed5707358d8363f1c29d7e8bbff9d62d1
Ethan Wortzman <ethanwortzman@g.harvard.edu>
2014-07-28 13:33:05 -0400

    updated sidebar template, added placeholders to dashboard sidebar

--- app/views/dashboard.blade.php
+++ app/views/dashboard.blade.php
@@ -7,6 +7,17 @@ Dashboard
 @extends('templates.with-sidebar')
 
 @section('sidebar')
+<ul class="nav">
+	<li>
+		<a href="#">link 1</a>
+	</li>
+	<li>
+		<a href="#">link 2</a>
+	</li>
+	<li>
+		<a href="#">link 3</a>
+	</li>
+</ul>
 @stop
 
 @section('jumbotron')
--- app/views/templates/with-sidebar.blade.php
+++ app/views/templates/with-sidebar.blade.php
@@ -2,12 +2,12 @@
 <div class="row row-offcanvas row-offcanvas-left">
       
 	<!-- sidebar -->
-  <div class="col-xs-6 col-sm-3 sidebar-offcanvas" id="sidebar" role="navigation">
+  <div class="col-xs-4 col-sm-2 sidebar-offcanvas" id="sidebar" role="navigation">
   @yield('sidebar')
   </div>
 	
   <!-- jumbotron -->
-  <div class="col-xs-12 col-sm-9">
+  <div class="col-xs-12 col-sm-10">
   	<div class="jumbotron">
     	<div class="container">
       @yield('jumbotron')
